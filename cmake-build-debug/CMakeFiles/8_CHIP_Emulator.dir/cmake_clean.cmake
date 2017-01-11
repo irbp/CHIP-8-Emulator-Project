e1a14467ffa271028fbaeb7825e9445c86848b77
Ítalo Rodrigo <irbp@cin.ufpe.br>
2017-01-11 02:29:27 -0300

    init method added

---
+++ cmake-build-debug/CMakeFiles/8_CHIP_Emulator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/8_CHIP_Emulator.dir/chip8Core.cpp.obj"
+  "8_CHIP_Emulator.pdb"
+  "8_CHIP_Emulator.exe"
+  "8_CHIP_Emulator.exe.manifest"
+  "lib8_CHIP_Emulator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8_CHIP_Emulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
